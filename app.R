6ad429f64c1f64613b232ef370bc2b0f0287dd74
Thomas D. Pellegrin <contact@theaviationdoctor.com>
2020-11-04 16:04:28 +0800

    Countries commit

--- app.R
+++ app.R
@@ -2,8 +2,11 @@
 # HOUSEKEEPING CODE                                                           #
 ###############################################################################
 
+# Install pacakges
+install.packages(tidyverse)
+
 # Load libraries
-library(shiny)
+library(shiny, tidyverse)
 
 # Clear the console
 cat("\014")
@@ -12,7 +15,7 @@ cat("\014")
 # VARIABLE DECLARATION                                                        #
 ###############################################################################
 
-#origins <- list("AF","AX","AL","DZ","AS","AD","AO","AI","AG","AR","AM","AW","AU","AT","AZ","BS","BH","BD","BB","BY","BE","BZ","BJ","BM","BT","BO","BQ","BA","BW","BV","BR","IO","BN","BG","BF","BI","CV","KH","CM","CA","KY","CF","TD","CL","CN","CX","CC","CO","KM","CG","CD","CK","CR","CI","HR","CU","CW","CY","CZ","DK","DJ","DM","DO","EC","EG","SV","GQ","ER","EE","ET","FK","FO","FJ","FI","FR","GF","PF","TF","GA","GM","GE","DE","GH","GI","GR","GL","GD","GP","GU","GT","GG","GN","GW","GY","HT","HM","VA","HN","HK","HU","IS","IN","ID","IR","IQ","IE","IM","IL","IT","JM","JP","JE","JO","KZ","KE","KI","KP","KR","XK","KW","KG","LA","LV","LB","LS","LR","LY","LI","LT","LU","MO","MK","MG","MW","MY","MV","ML","MT","MH","MQ","MR","MU","YT","MX","FM","MD","MC","MN","ME","MS","MA","MZ","MM","NA","NR","NP","NL","NC","NZ","NI","NE","NG","NU","NF","MP","NO","OM","PK","PW","PS","PA","PG","PY","PE","PH","PN","PL","PT","PR","QA","RE","RO","RU","RW","BL","SH","KN","LC","MF","PM","VC","WS","SM","ST","SA","SN","RS","SC","SL","SG","SX","SK","SI","SB","SO","ZA","GS","SS","ES","LK","SD","SR","SJ","SZ","SE","CH","SY","TW","TJ","TZ","TH","TL","TG","TK","TO","TT","TN","TR","TM","TC","TV","UG","UA","AE","GB","UM","US","UY","UZ","VU","VE","VN","VG","VI","WF","EH","YE","ZM","ZW")
+ origins <- list("Afghanistan","Albania","Algeria","Angola","Anguilla","Antigua and Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bonaire Saint Eustatius & Saba","Bosnia and Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei Darussalam","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central African Republic","Chad","Chile","China","Chinese Taipei","Cocos (Keeling) Islands","Colombia","Comoros","Congo","Cook Islands","Costa Rica","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Democratic Republic of the Congo","Denmark","Djibouti","Dominica","Dominican Republic","East Timor","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Fiji","Finland","France","French Guiana","French Polynesia","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada and South Grenadines","Guadeloupe","Guatemala","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong (SAR)","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Israel","Italy","Ivory Coast (Cote d'Ivoire)","Jamaica","Japan","Jordan","Kazakhstan","Kenya","Kiribati","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Libya","Lithuania","Luxembourg","Macau (SAR)","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Martinique","Mauritania","Mauritius","Mayotte","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Morocco","Mozambique","Myanmar","Namibia","Nauru","Nepal","Netherlands","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norfolk Island","North Korea","Norway","Oman","Pakistan","Palau","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Qatar","Reunion","Romania","Russian Federation","Rwanda","Saint Helena","Saint Kitts and Nevis","Saint Lucia","Saint Pierre and Miquelon","Saint Vincent and Grenadines","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Sint Maarten","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","Sudan","Suriname","Sweden","Switzerland","Syria","Tajikistan","Tanzania","Thailand","Togo","Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Turks and Caicos Islands","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","Uruguay","Uzbekistan","Vanuatu","Venezuela","Vietnam","Wallis and Futuna Islands","Western Sahara","Western Samoa","Yemen","Zambia","Zimbabwe")
 
 ###############################################################################
 # APPLICATION CODE                                                            #
@@ -35,11 +38,26 @@ if (interactive()) {
       
       # Sidebar panel for selecting inputs
       sidebarPanel(
-        selectInput(inputId = "CountryOrigin", label = "Country of origin", choices = origins), 
-        sliderInput(inputId = 'SampleSize', label = 'Sample size', min=0, max=2200000000000, value=2200000000000),
-        sliderInput(inputId = 'SamplePrevalence', label = 'Sample prevalence', min=0, max=1, value=.1),
-        sliderInput(inputId = 'TestSensitivity', label = 'Test sensitivity', min=0, max=1, value=.7),
-        sliderInput(inputId = 'TestSpecificity', label = 'Test specificity', min=0, max=1, value=.95)
+        # Country pair
+        h3("Origin characteristics"),
+        selectInput(inputId = "OriginState", label = "State of origin", choices = origins),
+        selectInput(inputId = "OriginPrevalenceChoice", label = "Disease prevalence at origin", choices = list("Automatic", "Manual")),
+        sliderInput(inputId = "OriginPrevalence", label = "", min=0, max=1, value=.1),
+        hr(),
+        h3("Destination characteristics"),
+        selectInput(inputId = "DestinationState", label = "State of destination", choices = origins),
+        selectInput(inputId = "DestinationPrevalenceChoice", label = "Disease prevalence at destination", choices = list("Automatic", "Manual")),
+        sliderInput(inputId = "DestinationPrevalence", label = "", min=0, max=1, value=.1),
+        hr(),
+        h3("Traffic volume"),
+        selectInput(inputId = "PassengerCountChoice", label = "Passenger count", choices = list("Automatic", "Manual")),
+        sliderInput(inputId = "PassengerCount", label = "", min=0, max=2200000000000, value=2200000000000),
+        # Test design
+        h3("Select a test design:"),
+        sliderInput(inputId = "LimitOfDetection", label = "Limit of detection (copies/ml)", min=0, max=10^3, value=10),
+        sliderInput(inputId = "SamplePrevalence", label = "Sample prevalence", min=0, max=1, value=.1),
+        sliderInput(inputId = "TestSensitivity", label = "Test sensitivity", min=0, max=1, value=.7),
+        sliderInput(inputId = "TestSpecificity", label = "Test specificity", min=0, max=1, value=.95)
       ),
       # Main panel for displaying outputs
       mainPanel(
