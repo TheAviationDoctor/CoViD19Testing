4e3253c3b252ab73a03303c6bc0fca4ecd6dfd45
Thomas D. Pellegrin <contact@theaviationdoctor.com>
2020-11-04 16:40:04 +0800

    Readme.md

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # CoViD19Testing
 A shiny app to test scenarios of COVID-19 passenger screening in an air travel context.
+
+This R app is meant to test different scenarios of passenger screening for COVID-19 at departure and/or arrival. It models the following variables:
+
+### Disease characteristics
+- Disease prevalence in the departure state (derived from published medical data, or entered manually by the user)
+- Disease prevalence in the arrival state (derived from published medical data, or entered manually by the user)
+- Viral load ramp-up (to detectability)
+
+### Passenger characteristics
+- Number of passengers traveling from the departure state to the arrival state (derived from AirportIS data, or entered manually by the user)
+- Proportion of passengers getting tested among them (i.e. systematic or sample-based testing)
+
+### Test characteristics
+- Limit of detection (analytical sensitivity), in viral copies per milliliter
+- Clinical sensitivity (true positives over all tests)
+- Clinical specificity (true negatives over all tests)
+
+### Operational characteristics
+- Whether the test is performed before departure, after arrival, or both
+- Time of testing relative to departure or arrival
--- app.R
+++ app.R
@@ -2,11 +2,9 @@
 # HOUSEKEEPING CODE                                                           #
 ###############################################################################
 
-# Install pacakges
-install.packages(tidyverse)
-
 # Load libraries
-library(shiny, tidyverse)
+library(shiny)
+library(tidyverse)
 
 # Clear the console
 cat("\014")
@@ -15,74 +13,80 @@ cat("\014")
 # VARIABLE DECLARATION                                                        #
 ###############################################################################
 
- origins <- list("Afghanistan","Albania","Algeria","Angola","Anguilla","Antigua and Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bonaire Saint Eustatius & Saba","Bosnia and Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei Darussalam","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central African Republic","Chad","Chile","China","Chinese Taipei","Cocos (Keeling) Islands","Colombia","Comoros","Congo","Cook Islands","Costa Rica","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Democratic Republic of the Congo","Denmark","Djibouti","Dominica","Dominican Republic","East Timor","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Fiji","Finland","France","French Guiana","French Polynesia","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada and South Grenadines","Guadeloupe","Guatemala","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong (SAR)","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Israel","Italy","Ivory Coast (Cote d'Ivoire)","Jamaica","Japan","Jordan","Kazakhstan","Kenya","Kiribati","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Libya","Lithuania","Luxembourg","Macau (SAR)","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Martinique","Mauritania","Mauritius","Mayotte","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Morocco","Mozambique","Myanmar","Namibia","Nauru","Nepal","Netherlands","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norfolk Island","North Korea","Norway","Oman","Pakistan","Palau","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Qatar","Reunion","Romania","Russian Federation","Rwanda","Saint Helena","Saint Kitts and Nevis","Saint Lucia","Saint Pierre and Miquelon","Saint Vincent and Grenadines","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Sint Maarten","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","Sudan","Suriname","Sweden","Switzerland","Syria","Tajikistan","Tanzania","Thailand","Togo","Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Turks and Caicos Islands","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","Uruguay","Uzbekistan","Vanuatu","Venezuela","Vietnam","Wallis and Futuna Islands","Western Sahara","Western Samoa","Yemen","Zambia","Zimbabwe")
+origins <- list("Afghanistan","Albania","Algeria","Angola","Anguilla","Antigua and Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bonaire Saint Eustatius & Saba","Bosnia and Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei Darussalam","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central African Republic","Chad","Chile","China","Chinese Taipei","Cocos (Keeling) Islands","Colombia","Comoros","Congo","Cook Islands","Costa Rica","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Democratic Republic of the Congo","Denmark","Djibouti","Dominica","Dominican Republic","East Timor","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Fiji","Finland","France","French Guiana","French Polynesia","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada and South Grenadines","Guadeloupe","Guatemala","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong (SAR)","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Israel","Italy","Ivory Coast (Cote d'Ivoire)","Jamaica","Japan","Jordan","Kazakhstan","Kenya","Kiribati","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Libya","Lithuania","Luxembourg","Macau (SAR)","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Martinique","Mauritania","Mauritius","Mayotte","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Morocco","Mozambique","Myanmar","Namibia","Nauru","Nepal","Netherlands","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norfolk Island","North Korea","Norway","Oman","Pakistan","Palau","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Qatar","Reunion","Romania","Russian Federation","Rwanda","Saint Helena","Saint Kitts and Nevis","Saint Lucia","Saint Pierre and Miquelon","Saint Vincent and Grenadines","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Sint Maarten","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","Sudan","Suriname","Sweden","Switzerland","Syria","Tajikistan","Tanzania","Thailand","Togo","Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Turks and Caicos Islands","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","Uruguay","Uzbekistan","Vanuatu","Venezuela","Vietnam","Wallis and Futuna Islands","Western Sahara","Western Samoa","Yemen","Zambia","Zimbabwe")
 
 ###############################################################################
 # APPLICATION CODE                                                            #
 ###############################################################################
 
 if (interactive()) {
-  options(device.ask.default = FALSE)
-  
-  #############################################################################
-  # USER INTERFACE LOGIC                                                      #
-  #############################################################################
-  
-  ui <- fluidPage(
+    options(device.ask.default = FALSE)
     
-    # App title
-    titlePanel("SARS-CoV-2 Air Travel Screening Simulator"),
+    #############################################################################
+    # USER INTERFACE LOGIC                                                      #
+    #############################################################################
     
-    # Layout with input and output definitions
-    sidebarLayout(
-      
-      # Sidebar panel for selecting inputs
-      sidebarPanel(
-        # Country pair
-        h3("Origin characteristics"),
-        selectInput(inputId = "OriginState", label = "State of origin", choices = origins),
-        selectInput(inputId = "OriginPrevalenceChoice", label = "Disease prevalence at origin", choices = list("Automatic", "Manual")),
-        sliderInput(inputId = "OriginPrevalence", label = "", min=0, max=1, value=.1),
-        hr(),
-        h3("Destination characteristics"),
-        selectInput(inputId = "DestinationState", label = "State of destination", choices = origins),
-        selectInput(inputId = "DestinationPrevalenceChoice", label = "Disease prevalence at destination", choices = list("Automatic", "Manual")),
-        sliderInput(inputId = "DestinationPrevalence", label = "", min=0, max=1, value=.1),
-        hr(),
-        h3("Traffic volume"),
-        selectInput(inputId = "PassengerCountChoice", label = "Passenger count", choices = list("Automatic", "Manual")),
-        sliderInput(inputId = "PassengerCount", label = "", min=0, max=2200000000000, value=2200000000000),
-        # Test design
-        h3("Select a test design:"),
-        sliderInput(inputId = "LimitOfDetection", label = "Limit of detection (copies/ml)", min=0, max=10^3, value=10),
-        sliderInput(inputId = "SamplePrevalence", label = "Sample prevalence", min=0, max=1, value=.1),
-        sliderInput(inputId = "TestSensitivity", label = "Test sensitivity", min=0, max=1, value=.7),
-        sliderInput(inputId = "TestSpecificity", label = "Test specificity", min=0, max=1, value=.95)
-      ),
-      # Main panel for displaying outputs
-      mainPanel(
-        ls(all.names = TRUE),
-        textOutput("ProbabilityPositiveGivenInfected")
-      )
+    ui <- fluidPage(
+        
+        # App title
+        titlePanel("SARS-CoV-2 Air Travel Screening Simulator"),
+        
+        # Layout with input and output definitions
+        sidebarLayout(
+            
+            # Sidebar panel for selecting inputs
+            sidebarPanel(
+                # Departure state characteristics
+                h3("Departure state characteristics"),
+                selectInput(inputId = "DepartureState", label = "State of origin", choices = origins),
+                selectInput(inputId = "DepartureStatePrevalenceChoice", label = "Disease prevalence in the departure state", choices = list("Automatic (assumed from medical data)", "Manual (override with your own)")),
+                sliderInput(inputId = "DepartureStatePrevalence", label = "", min=0, max=1, value=.1),
+
+                # Arrival state characteristics
+                hr(),
+                h3("Arrival state characteristics"),
+                selectInput(inputId = "ArrivalState", label = "State of destination", choices = origins),
+                selectInput(inputId = "ArrivalStatePrevalenceChoice", label = "Disease prevalence in the arrival state", choices = list("Automatic (assumed from medical data)", "Manual (override with your own)")),
+                sliderInput(inputId = "ArrivalStatePrevalence", label = "", min=0, max=1, value=.1),
+
+                # Population characteristics
+                hr(),
+                h3("Population characteristics"),
+                selectInput(inputId = "PopulationCountChoice", label = "Passenger count", choices = list("Automatic (based on AirportIS data)", "Manual (override with your own)")),
+                sliderInput(inputId = "PopulationCount", label = "", min=0, max=4.5*10^9, value=2.2*10^9),
+                sliderInput(inputId = "PopulationTestingRate", label = "Proportion of passengers being tested", min=0, max=100, value=100),
+
+                # Test characteristics
+                hr(),
+                h3("Test characteristics"),
+                sliderInput(inputId = "TestLimitOfDetection", label = "Limit of detection (copies/ml)", min=0, max=10^4, value=1000),
+                sliderInput(inputId = "TestSensitivity", label = "Test sensitivity", min=0, max=1, value=.7),
+                sliderInput(inputId = "TestSpecificity", label = "Test specificity", min=0, max=1, value=.95)
+            ),
+            # Main panel for displaying outputs
+            mainPanel(
+                ls(all.names = TRUE),
+                textOutput("ProbabilityPositiveGivenInfected")
+            )
+        )
     )
-  )
-
-  #############################################################################
-  # SERVER LOGIC                                                              #
-  #############################################################################
-  
-  server <- function(input, output) {
     
-    output$ProbabilityPositiveGivenInfected <- renderText({ input$TestSensitivity })
-    # output$ProbabilityPositiveGivenNotInfected <- renderText({ 1- input$TestSpecificity })
-    # output$ProbabilityNegativeGivenInfected <- renderText({ 1 - input$TestSensitivity })
-    # output$ProbabilityNegativeGivenNotInfected <- renderText({ input$TestSpecitificity })
+    #############################################################################
+    # SERVER LOGIC                                                              #
+    #############################################################################
     
-  }
-
-  #############################################################################
-  # RUN APPLICATION                                                           #
-  #############################################################################
-  
-  shinyApp(ui, server)
+    server <- function(input, output) {
+        
+        output$ProbabilityPositiveGivenInfected <- renderText({ input$TestSensitivity })
+        # output$ProbabilityPositiveGivenNotInfected <- renderText({ 1- input$TestSpecificity })
+        # output$ProbabilityNegativeGivenInfected <- renderText({ 1 - input$TestSensitivity })
+        # output$ProbabilityNegativeGivenNotInfected <- renderText({ input$TestSpecitificity })
+        
+    }
+    
+    #############################################################################
+    # RUN APPLICATION                                                           #
+    #############################################################################
+    
+    shinyApp(ui, server)
 }
